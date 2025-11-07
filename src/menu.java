510e5a9d7cd217e08cd4c2b341b8b7f694c1ed80
joaopedr0-rgb <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>
2025-11-07 14:51:04 -0300

    terceiro commit

--- src/menu.java
+++ src/menu.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import packCarros.carros;
+
 public class menu {
 
     //importações 
@@ -9,21 +10,28 @@ public class menu {
 
 
     public static void main(String[] args) {
+        
+
+
+
+
         // Variaveis usadas para guardar informações do usuario
         // Tambem usadas de parametro nas funções login
-        
         String User = null;
         String senha= null;
 
-        //prints usados para fazer area inicial do sistema
+
+         //prints usados para fazer area inicial do sistema
         System.out.println("Faça login ou cadastro ");
         System.out.println("1. Login:");
         System.out.println("2. Cadastro:");
+
+
         //Variavel usada para controle no switch
-
-
-
         int opcao = escaneador.nextInt();
+
+
+
         switch(opcao){
             case 1:
             {
@@ -57,13 +65,23 @@ public class menu {
 
 
     }
+
+
+
+
+
+
     //
     public static void Cadastro(String CadUser, String CadSenha){
         //Variavel "verefique" usada para verificar se a senha está correta
         String Verefique = null;
+
+
         //variaveis "contador" e "i"usadas para controlar o While
         int contador = 10;
         int i = 0;
+
+
         //While seraa usado para reiniciar a area de cadastro caso o usuario falhe na verificaçao de senha
         while (i < contador ){
             System.out.println("Digite seu nome de usuario:");
@@ -87,18 +105,66 @@ public class menu {
 
 
     }
+
+
+
+
     public static void Menu( ){
         System.out.println( "                                MENU PRINCIPAL");
 
-        int opcaoMenu;
+        int opcaoMenu = escaneador.nextInt();
 
-          System.out.print("\033[H\033[2J");
-        System.out.flush();
+       
 
         System.out.println("\n Escolha alguma das opções abaixo:");
         System.out.println("1. Gerenciar veiculos.");
         System.out.println("2. Gerenciamento de clientes.");
         System.out.println("3. Controle de vendas.");
+        
+        switch(opcaoMenu){
+
+            case 1:
+            {
+                GerenciaCarros();
+            }
+
+        }
+    }
+
+
+
+
+
+    public static void GerenciaCarros(){
+
+        carros carro1 = new carros();
+        carro1.modelo = "fusca";
+
+        int CarsControler = escaneador.nextInt();
+
+        System.out.println(carro1.modelo);
+
+        System.out.println(" \n Gerenciamento de vendas:");
+        System.out.println("1. Cadastro veículo novo.");
+        System.out.println("2. Ver veiculos.");
+        System.out.println("3. Editar veiculo.");
+        System.out.println("4. Exluir cadastro de veiculos.");
+        System.out.println("0. Voltar para o menu principal.");
+
+        switch(CarsControler){
+
+            case 0:
+            {
+                Menu();
+            }
+
+            case 1:
+            {
+                System.out.println("\n    Cadastro de veiculos");
+                
+
+            }
+        }
 
     }
 }
